3ca5f681bd87809a4dd4ad8761afb1ef52748936
Ernst Rijsdijk <ernst.rijsdijk@holder.nl>
2011-11-10 11:43:17 +0100

    Add global error messages

--- lib/twinfield.rb
+++ lib/twinfield.rb
@@ -11,4 +11,18 @@ module Twinfield
     :process => "/webservices/processxml.asmx?wsdl"
     :finder => "/webservices/finder.asmx?wsdl"
   }
+
+  ERRORS = {
+    100 => "Unexpected exception.",
+    101 => "Session header is missing.",
+    102 => "Access is denied.",
+    103 => "The log-on credentials are not valid anymore.",
+    104 => "The log-on has been deleted.",
+    105 => "The log-on has been disabled.",
+    106 => "The organisation is no longer active.",
+    107 => "SMS failed to send.",
+    108 => "Access to this server is not allowed because the cluster is invalid.",
+    109 => "You need access to at least one company to log on.",
+    110 => "Login is not allowed on this server"
+  }
 end
