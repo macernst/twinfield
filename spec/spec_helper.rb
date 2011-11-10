7197bf26e8310e06b9186535e36543e60ca11eeb
Ernst Rijsdijk <ernst.rijsdijk@holder.nl>
2011-11-10 17:41:54 +0100

    Remove old information

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,9 @@ require File.expand_path('../../lib/twinfield', __FILE__)
 
 def reset_config
   Twinfield.configure do |config|
-    config.username = "NL000078"
-    config.password = "yvy8116ume"
-    config.organisation = "TWF-SAAS"
+    config.username = ""
+    config.password = ""
+    config.organisation = ""
   end
 end
 
